76fa9c93cc83c914a1e1d76ad1027d75a1c92a7b
annve <ann.vaneyken@betavzw.org>
2018-02-12 15:52:32 +0100

    app.module.ts aangepast voor oefheroes

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -1,29 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';  // nodig voor ngModel (-> 2 way binding)
-
-import { AppComponent } from './app.component';
-import { Oef00Component } from './oefeningen/oef00/oef00.component';
-import { OefNotitieComponent } from './oefeningen/oefnotitie/oefnotitie.component';
-import { VbForuitbrComponent } from './voorbeelden/vbfor/vbforuitbr.component';
-import { OefBroodjesComponent } from './oefeningen/oefbroodjes/oefbroodjes.component';
-import { OefHogerLagerComponent } from './oefeningen/oefhogerlager/oefhogerlager.component';
-import { OefCurrencyconvertorComponent } from './oefeningen/oefcurrencyconvertor/oefcurrencyconvertor.component';
-import { CurrencyService } from './oefeningen/oefcurrencyconvertor/currency.service';
 import { HttpClientModule } from '@angular/common/http';
-import { Routes, RouterModule } from '@angular/router';
-import { VbDatabindingComponent } from './voorbeelden/vbdatabinding/vbdatabinding.component';
-import { VbIfComponent } from './voorbeelden/vbif/vbif.component';
-import { VbroutingReqparamComponent } from './voorbeelden/vbrouting/vbrouting-reqparam.component';
-import { PageNotFoundComponent } from './voorbeelden/vbrouting/page-not-found.component';
-import { Vbrouting0Component } from './voorbeelden/vbrouting/vbrouting0.component';
-import { VbroutingComponent } from './voorbeelden/vbrouting/vbrouting.component';
+//import { Routes, RouterModule } from '@angular/router';
+
+// import { AppComponent } from './app.component';
+// import { Oef00Component } from './oefeningen/oef00/oef00.component';
+// import { OefNotitieComponent } from './oefeningen/oefnotitie/oefnotitie.component';
+// import { VbForuitbrComponent } from './voorbeelden/vbfor/vbforuitbr.component';
+// import { OefBroodjesComponent } from './oefeningen/oefbroodjes/oefbroodjes.component';
+// import { OefHogerLagerComponent } from './oefeningen/oefhogerlager/oefhogerlager.component';
+// import { OefCurrencyconvertorComponent } from './oefeningen/oefcurrencyconvertor/oefcurrencyconvertor.component';
+// import { CurrencyService } from './oefeningen/oefcurrencyconvertor/currency.service';
+// import { VbDatabindingComponent } from './voorbeelden/vbdatabinding/vbdatabinding.component';
+// import { VbIfComponent } from './voorbeelden/vbif/vbif.component';
+// import { VbroutingReqparamComponent } from './voorbeelden/vbrouting/vbrouting-reqparam.component';
+// import { PageNotFoundComponent } from './voorbeelden/vbrouting/page-not-found.component';
+// import { Vbrouting0Component } from './voorbeelden/vbrouting/vbrouting0.component';
+// import { VbroutingComponent } from './voorbeelden/vbrouting/vbrouting.component';
 import { HeroesService } from './oefeningen/oefheroes/heroes.service';
 import { HeroesComponent } from './oefeningen/oefheroes/heroes/heroes.component';
 import { HeroesAllComponent } from './oefeningen/oefheroes/heroes-all/heroes-all.component';
 import { HeroDetailComponent } from './oefeningen/oefheroes/hero-detail/hero-detail.component';
 // TODO: alle componenten die in deze module gedefinieerd zijn importere
-  // zie vbrouting.module.ts
+
+// zie vbrouting.module.ts
   /*
 const appRoutes: Routes = [
   {
@@ -59,13 +60,6 @@ const appRoutes: Routes = [
 
 @NgModule({
   declarations: [
-    VbDatabindingComponent,
-    VbIfComponent,
-    OefCurrencyconvertorComponent,
-    PageNotFoundComponent,
-    Vbrouting0Component,
-    VbroutingComponent,
-    VbroutingReqparamComponent, 
     HeroDetailComponent,
     HeroesComponent,
     HeroesAllComponent,
@@ -76,15 +70,11 @@ const appRoutes: Routes = [
     FormsModule,  //  nodig om 2 way binding te kunnen doen
     HttpClientModule,
     // RouterModule.forRoot( appRoutes ),
-    VbRoutingModule
+//    VbRoutingModule
   ],
   providers: [HeroesService],
   // TODO: in bootstrap array (enkel) alle componenten opsommen
   // die horen bij user defined tags die in index.html gebruikt worden
-<<<<<<< HEAD
-  bootstrap: [VbroutingComponent]
-=======
   bootstrap: [HeroesAllComponent]
->>>>>>> e4628f31f85764904bdcc0f100ad115a8ddbea86
 })
 export class AppModule { }
